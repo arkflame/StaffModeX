8822b4e4a40fe26635e53fc2c1d15422022c23fe
linsaftw <linsaftw@users.noreply.github.com>
2024-05-21 19:07:13 -0300

    configurable potion effects

--- src/main/java/com/arkflame/staffmodex/managers/StaffModeManager.java
+++ src/main/java/com/arkflame/staffmodex/managers/StaffModeManager.java
@@ -10,10 +10,12 @@ import com.arkflame.staffmodex.modernlib.utils.PotionEffects;
 import com.arkflame.staffmodex.player.StaffPlayer;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 
 public class StaffModeManager {
     private final Collection<Player> staffPlayers = new HashSet<>();
@@ -27,6 +29,53 @@ public class StaffModeManager {
             addStaff(player);
             Bukkit.getScheduler().runTaskAsynchronously(StaffModeX.getInstance(),
                     () -> StaffModeX.getInstance().getRedisManager().addPlayerToStaffMode(player.getName()));
+        }
+    }
+
+    public void applyPotionEffects(Player player) {
+        ConfigWrapper cfg = StaffModeX.getInstance().getCfg();
+        ConfigurationSection effectsSection = cfg.getConfigurationSection("staffmode.effects");
+    
+        if (effectsSection != null) {
+            for (String effectName : effectsSection.getKeys(false)) {
+                ConfigurationSection effectSection = effectsSection.getConfigurationSection(effectName);
+                if (effectSection != null) {
+                    int amplifier = effectSection.getInt("amplifier", 1);
+                    int duration = effectSection.getInt("duration", Integer.MAX_VALUE);
+    
+                    List<String> aliases = effectSection.getStringList("aliases");
+                    String[] effects = aliases.toArray(new String[0]);
+    
+                    // Apply the potion effects using your API
+                    PotionEffects.add(player, amplifier, duration, effects);
+                } else {
+                    StaffModeX.getInstance().getLogger().warning("Invalid configuration section for effect: " + effectName);
+                }
+            }
+        } else {
+            StaffModeX.getInstance().getLogger().warning("No potion effects found in the configuration.");
+        }
+    }
+
+    public void removePotionEffects(Player player) {
+        ConfigWrapper cfg = StaffModeX.getInstance().getCfg();
+        ConfigurationSection effectsSection = cfg.getConfigurationSection("staffmode.effects");
+
+        if (effectsSection != null) {
+            for (String effectName : effectsSection.getKeys(false)) {
+                ConfigurationSection effectSection = effectsSection.getConfigurationSection(effectName);
+                if (effectSection != null) {
+                    List<String> aliases = effectSection.getStringList("aliases");
+                    String[] effects = aliases.toArray(new String[0]);
+
+                    // Remove the potion effects using your API
+                    PotionEffects.remove(player, effects);
+                } else {
+                    StaffModeX.getInstance().getLogger().warning("Invalid configuration section for effect: " + effectName);
+                }
+            }
+        } else {
+            StaffModeX.getInstance().getLogger().warning("No potion effects found in the configuration.");
         }
     }
 
@@ -60,9 +109,7 @@ public class StaffModeManager {
         staffPlayers.add(player);
 
         // Potion Effects
-        PotionEffects.add(player, 0, Integer.MAX_VALUE, "NIGHT_VISION");
-        PotionEffects.add(player, 1, Integer.MAX_VALUE, "JUMP", "JUMP_BOOST");
-        PotionEffects.add(player, 1, Integer.MAX_VALUE, "SPEED");
+        applyPotionEffects(player);
 
         // Armor
         StaffModeX.getInstance().getArmorManager().giveArmor(player);
@@ -83,9 +130,9 @@ public class StaffModeManager {
         ConfigWrapper msg = StaffModeX.getInstance().getMsg();
         player.sendMessage(ChatColors.color(msg.getText("staffmode.leave")));
         staffPlayers.remove(player);
-        PotionEffects.remove(player, "NIGHT_VISION");
-        PotionEffects.remove(player, "JUMP", "JUMP_BOOST");
-        PotionEffects.remove(player, "SPEED");
+
+        // Remove potion effects
+        removePotionEffects(player);
 
         StaffPlayer staffPlayer = StaffModeX.getInstance().getStaffPlayerManager().getOrCreateStaffPlayer(player);
         if (staffPlayer != null) {
