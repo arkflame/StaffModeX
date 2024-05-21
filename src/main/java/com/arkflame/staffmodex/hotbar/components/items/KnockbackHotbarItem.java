69aacc780a93d3efbcc25fa2e56d4a52a499db2e
linsaftw <linsaftw@users.noreply.github.com>
2024-05-21 19:11:46 -0300

    knockback added to knockback item

--- src/main/java/com/arkflame/staffmodex/hotbar/components/items/KnockbackHotbarItem.java
+++ src/main/java/com/arkflame/staffmodex/hotbar/components/items/KnockbackHotbarItem.java
@@ -1,5 +1,7 @@
 package com.arkflame.staffmodex.hotbar.components.items;
 
+import org.bukkit.enchantments.Enchantment;
+
 import com.arkflame.staffmodex.StaffModeX;
 import com.arkflame.staffmodex.hotbar.HotbarItem;
 import com.arkflame.staffmodex.modernlib.utils.Materials;
@@ -10,5 +12,11 @@ public class KnockbackHotbarItem extends HotbarItem {
                 StaffModeX.getInstance().getMsg().getText("hotbar.knockback.name"),
                 1, (short) 0,
                 StaffModeX.getInstance().getMsg().getTextList("hotbar.knockback.lore"));
+
+        try {
+            addEnchantment(Enchantment.KNOCKBACK, 1);
+        } catch (Exception ex) {
+            // Catch for invalid enchantment in new versions
+        }
     }
 }
